b84bfa5d1934f552eebd4f5a7372f2cb6c3eb40f
Matias Benavente <benaventematias57@hotmail.com>
2022-03-29 12:41:06 -0300

    Ahora si funciona la API

--- src/main/java/com/pw/api/RegisterUserAPI.java
+++ src/main/java/com/pw/api/RegisterUserAPI.java
@@ -1,5 +1,7 @@
 package com.pw.api;
 
+import javax.annotation.Resource;
+
 import com.pw.DTO.ContactDTO;
 import com.pw.logic.Contact;
 import com.pw.persistance.DBController;
@@ -9,23 +11,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegisterUserAPI {
 
     @Autowired
-    private DBController DB = new DBController();
+    private DBController DB;
 
     @PostMapping("/agenda")
     public HttpStatus registrar(@RequestBody ContactDTO contact){
 
-        return DB.register(contact.getName(),Integer.parseInt(contact.getNumber()),contact.getMail());
+        if(resource.validateEmail(contact.getMail()){
+            return DB.register(contact.getName(),Integer.parseInt(contact.getNumber()),contact.getMail());
+        }
     }
 
     @GetMapping("/agenda")
-    public Contact search(@RequestHeader("name") String name){
+    public Contact search(@RequestParam(value="name") String name){
          
         return DB.search(name);
     }
--- src/main/java/com/pw/persistance/DBController.java
+++ src/main/java/com/pw/persistance/DBController.java
@@ -4,24 +4,26 @@ import com.pw.logic.Contact;
 import com.pw.logic.ContactMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
 
-@Service
-@Configuration
+@Repository("DBController")
 public class DBController {
 
-    @Autowired
-    private static JdbcTemplate db;
+private JdbcTemplate db;
 
-    @Bean
-    public static HttpStatus register(String name, int number, String mail){
+public JdbcTemplate getDB() {
+    return db;
+}
+
+@Autowired
+public void setDB(JdbcTemplate db) {
+    this.db = db;
+}
+
+    public HttpStatus register(String name, int number, String mail){
         try {
             String statement = "INSERT INTO PHONEBOOK(nameR, num, mail) VALUES('" + name + "'," + number + ",'" + mail + "')";
             db.update(statement);
@@ -31,10 +33,10 @@ public class DBController {
         }
     }
     
-    @Bean
-    public static Contact search(String name){
-        String query="SELECT * FROM POHONEBOOK WHERE nameR="+name;
+    public Contact search(String name){
+        String query="SELECT * FROM PHONEBOOK WHERE nameR='"+name+"'";
         Contact con = new Contact();
+
         con = (db.query(query, new ContactMapper())).get(0);
         return con;
     }
